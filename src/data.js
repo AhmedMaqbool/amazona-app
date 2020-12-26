7f7e5471098ebfca4c193ca297bd41914b0a4911
Ahmed Maqbool <39122332+AhmedMaqbool@users.noreply.github.com>
2020-12-26 20:14:18 +0300

    inital commit

---
+++ src/data.js
@@ -0,0 +1,85 @@
+import img1 from './images/p1.jpg'
+import img2 from './images/p2.jpg'
+import img3 from './images/p3.jpg'
+import img4 from './images/p4.jpg'
+import img5 from './images/p5.jpg'
+import img6 from './images/p6.jpg'
+
+const data={
+    products:[
+        {
+           _id:1,
+            name:"Nike Slim Shirt",
+            category:'Shirt',
+            image:img1,
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:"hight quality product"
+
+        },
+        {
+           _id:2,
+            name:"Adidas Fit Shirt",
+            category:'Shirt',
+            image:img2,
+            price:100,
+            brand:'Addidas',
+            rating:4,
+            numReviews:10,
+            description:"hight quality product"
+
+        },
+        {
+           _id:3,
+            name:"Lacoste Slim Shirt",
+            category:'Shirt',
+            image:img3,
+            price:220,
+            brand:'Lacoste',
+            rating:3.5,
+            numReviews:10,
+            description:"hight quality product"
+
+        },
+        {
+           _id:4,
+            name:"Puma Slim Shirt",
+            category:'Pants',
+            image:img4,
+            price:2000,
+            brand:'Puma',
+            rating:5,
+            numReviews:10,
+            description:"hight quality product"
+
+        },
+        {
+           _id:5,
+            name:"New Balance Shirt",
+            category:'Shirt',
+            image:img5,
+            price:480,
+            brand:'New Balance',
+            rating:4,
+            numReviews:10,
+            description:"hight quality product"
+
+        },
+        {
+           _id:6,
+            name:"Reebok Slim Pant",
+            category:'Shirt',
+            image:img6,
+            price:1890,
+            brand:'Rebook',
+            rating:3.2,
+            numReviews:10,
+            description:"hight quality product"
+
+        }
+    ]
+};
+ 
+export default data;
